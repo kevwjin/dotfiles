83e143ecf0b560873c71a5a9aea2d9245c128f43
Kevin Jin <59934204+kevwjin@users.noreply.github.com>
2025-01-14 14:07:34 -0800

    Add arrow.nvim plugin for buffer jumping

---
+++ dot_config/nvim/lua/plugins/arrow.lua
@@ -0,0 +1,9 @@
+return {
+  "otavioschwanck/arrow.nvim",
+  dependencies = {},
+  opts = {
+    show_icons = false,
+    leader_key = ';', -- Recommended to be a single key
+    buffer_leader_key = 'm', -- Per Buffer Mappings
+  }
+}
