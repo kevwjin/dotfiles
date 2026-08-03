73ccb7f5eed183522e5100501eacb7b49bf6fc1e
Kevin Jin <59934204+kevwjin@users.noreply.github.com>
2026-08-03 14:17:07 -0700

    feat(nvim): improve navigation and interface styling

--- dot_config/nvim/lua/plugins/arrow.lua
+++ dot_config/nvim/lua/plugins/arrow.lua
@@ -6,5 +6,25 @@ return {
     buffer_leader_key = 'm',  -- per buffer mark
     show_icons = false,
     hide_handbook = true,     -- hide keybind handbook
-  }
+  },
+  config = function(_, opts)
+    local save_keys = require("arrow.save_keys")
+    local default_cwd = save_keys.cwd
+
+    save_keys.cwd = function()
+      local cwd = default_cwd()
+      if cwd and cwd ~= "" then
+        return cwd
+      end
+
+      cwd = vim.fn.getcwd()
+      if cwd and cwd ~= "" then
+        return cwd
+      end
+
+      return vim.env.PWD or vim.env.HOME or "."
+    end
+
+    require("arrow").setup(opts)
+  end,
 }
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -21,20 +21,90 @@ return {
 
     local keymap = vim.keymap
     local builtin = require('telescope.builtin')
+
+    local function git_root_dir()
+      local git_root = vim.fs.dirname(vim.fs.find('.git', { upward = true })[1] or '')
+      if git_root == '' then
+        return nil
+      end
+
+      return git_root
+    end
+
+    local function git_root_search_dirs()
+      local git_root = git_root_dir()
+      if git_root == nil then
+        return nil
+      end
+
+      return { git_root }
+    end
+
+    local function exclude_gitignore()
+      return { '--no-ignore', '--hidden', '--glob', '!.gitignore' }
+    end
+
+    local function exclude_docs_dirs()
+      return { '!docs/**', '!**/docs/**' }
+    end
+
+    local function live_grep_git_tracked_files()
+      builtin.live_grep({
+        additional_args = exclude_gitignore,
+        search_dirs = git_root_search_dirs(),
+        glob_pattern = exclude_docs_dirs(),
+      })
+    end
+
+    local function current_token()
+      local line = vim.api.nvim_get_current_line()
+      local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+      local token_pattern = '[%a_][%w_]*'
+      local start_col, end_col = nil, nil
+
+      for s, e in line:gmatch('()' .. token_pattern .. '()') do
+        if s <= col and col < e then
+          start_col = s
+          end_col = e
+          break
+        end
+      end
+
+      if start_col == nil then
+        return vim.fn.expand('<cword>')
+      end
+
+      return line:sub(start_col, end_col - 1)
+    end
+
+    local function grep_git_tracked_word()
+      local search = current_token()
+
+      builtin.grep_string({
+        search = search,
+        use_regex = false,
+        additional_args = exclude_gitignore,
+        search_dirs = git_root_search_dirs(),
+        glob_pattern = exclude_docs_dirs(),
+      })
+    end
+
     keymap.set('n', '<leader>fd', function()
         builtin.find_files({
             prompt_title = "< Directory Search >",
+            cwd = git_root_dir(),
             find_command = { "find", ".", "-type", "d" }
         })
     end, { desc = 'Search for file dirs under project dir' })
-    keymap.set('n', '<leader>ff', builtin.find_files,
-      { desc = 'Search for file names under project dir' })
-    keymap.set('n', '<leader>fp', builtin.live_grep,
-      { desc = 'Search for string in file contents under project dir' })
+    keymap.set('n', '<leader>ff', function()
+      builtin.find_files({ cwd = git_root_dir() })
+    end, { desc = 'Search for file names under project dir' })
+    keymap.set('n', '<leader>fp', live_grep_git_tracked_files,
+      { desc = 'Search for string in repo files' })
     keymap.set('n', '<leader>fs', builtin.buffers,
       { desc = 'Search for file names in open buffers' })
-    keymap.set('n', '<leader>fw', builtin.grep_string,
-      { desc = 'Search for current word in file contents under project dir' })
+    keymap.set('n', '<leader>fw', grep_git_tracked_word,
+      { desc = 'Search for current token in repo files' })
     keymap.set('n', '<leader>fo', builtin.oldfiles,
       { desc = 'Search for file names in file history' })
     keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find,
